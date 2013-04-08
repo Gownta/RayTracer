6ef59d5e11283884b6e1654a71a32bcaf4c4150d
Nicholas Ormrod <nicholas.ormrod@hotmail.com>
2013-04-08 04:01:32 -0700

    random anti-aliasing image

--- src/scene.lua
+++ src/scene.lua
@@ -1,140 +1,123 @@
--- Coca Cola
+-- DNA
 
 scene = gr.node('scene')
+math.randomseed(95864752)
 
 -------------------
--- Materials
+-- Material
 
-silver= gr.material({0.6,0.6,0.6},{0.9,0.9,0.9},25)
+red   = gr.material({1,0,0},{1.0,0.2,0.2}, 25)
+green = gr.material({0.1,0.9,0.1},{0.2,1.0,0.2}, 25)
+blue  = gr.material({0,0.1,1},{0.2,0.2,1.0}, 15)
 
--------------------
--- Can
+pink  = gr.material({1,0.2,0.2},{1.0,0.2,0.2}, 25)
+orange= gr.material({1,0.6,0},{0.7,0.4,0.2}, 25)
+purple= gr.material({0.6,0.2,1},{0.2,0.2,0.2},25)
+magenta=gr.material({0.8,0,0.4},{0.2,0.3,0.1},25)
+yellow= gr.material({1,0.8,0.1},{0.8,0.8,0.8},25)
+floor = gr.material({0.8,0.8,0.8},{0,0,0}, 1, 0.7)
 
-v = {}
-n = {}
-t = {}
-f = {}
-
-tw = 1400
-th = 740
-
-h    = 2*1.618
-diph = 0.2
-dipa = 25 * (2 * 3.1415926 / 360)
-dipw = 1 - math.tan(dipa)*diph
-
-divs = 36
-for i = 0,divs do
-  angle = i * (2 * 3.14159265) / divs
-  ca = math.cos(angle)
-  sa = math.sin(angle)
-
-  table.insert(v, {ca,0,sa})
-  table.insert(v, {ca,h-diph-0.05,sa})
-  table.insert(v, {ca,h-diph,sa})
-  table.insert(v, {ca*dipw,h,sa*dipw})
-
-  table.insert(n, {ca,0,sa})
-  table.insert(n, {ca,0,sa})
-  table.insert(n, {ca*ca,sa,sa*ca})
-  table.insert(n, {ca*ca,sa,sa*ca})
-
-  table.insert(t, {i/divs*tw,0,0})
-  table.insert(t, {i/divs*tw,(1 - (diph + 0.05) / h)*th,0})
-  table.insert(t, {i/divs*tw,(1 - diph / h)*th,0})
-  table.insert(t, {i/divs*tw,th,0})
-end
-
-for j = 0,divs-1 do
-  i = 4*j
-  table.insert(f, {{i+0,i+1,i+5,i+4},{i+0,i+1,i+5,i+4},{i+0,i+1,i+5,i+4}})
-  table.insert(f, {{i+1,i+2,i+6,i+5},{i+1,i+2,i+6,i+5},{i+1,i+2,i+6,i+5}})
-  table.insert(f, {{i+2,i+3,i+7,i+6},{i+2,i+3,i+7,i+6},{i+2,i+3,i+7,i+6}})
-end
-
-coke = gr.mesh('can of coke', v, n, t, f)
-coke:set_material(gr.texture_material('coke.png', {0,0,0}, 1))
-
-blip = gr.algebraic('torus', '(x^2 + y^2 + z^2 + (1-0.1)^2 - (0.1)^2)^2 - 4*(1-0.1)^2*(x^2 + y^2)', 1)
-blip:set_material(silver)
-blip:scale(0.99,.99,.99)
-blip:rotate('X',90)
-
-tlip = gr.algebraic('torus', '(x^2 + y^2 + z^2 + (1-0.05)^2 - (0.05)^2)^2 - 4*(1-0.05)^2*(x^2 + y^2)', 1)
-tlip:set_material(silver)
-tlip:rotate('X',90)
-tlip:scale(dipw,dipw,dipw)
-tlip:translate(0,h,0)
-
-ctv = {}
-ctt = {}
-col = {}
-for i = 1,divs do
-  angle = i * (2 * 3.14159265) / divs
-  ca = math.cos(angle)
-  sa = math.sin(angle)
-  table.insert(ctv, {ca,0,sa})
-  table.insert(ctt, {200+ca*175,200+sa*175,0})
-  table.insert(col, i-1)
-end
-coketop = gr.mesh('coketop', ctv, {}, ctt, {{col,{},col}})
-coketop:set_material(gr.texture_material('coketop.png', {0.8,0.8,0.8},25))
-coketop:scale(dipw,dipw,dipw)
-coketop:translate(0,h-0.05,0)
-
-can = gr.node('can of coke')
-can:translate(0,0.1,0)
-can:add_child(coke)
-can:add_child(blip)
-can:add_child(tlip)
-can:add_child(coketop)
+lb = gr.material({0,0.4,0.8},{0.1,0.5,0.7},25)
+db = gr.material({0,0.2,0.6},{0.1,0.5,0.7},25)
 
 -------------------
--- the floor
+-- Cylinders
 
-ice = gr.mesh('ice',
-  { -- vertices
-    {-5, 0, -3.75},
-    { 5, 0, -3.75},
-    { 5, 0,  3.75},
-    {-5, 0,  3.75},
-  }, { -- normals
-  }, { -- textures
-    {0,0,0},
-    {500-1,0,0},
-    {500-1,375-1,0},
-    {0,375-1,0},
-  }, { -- faces
-    {
-      {0,1,2,3},
-      {},
-      {0,1,2,3}
-    }
-  })
-ice:set_material(gr.texture_material('ice.png', {0,0,0}, 1))
-ice:translate(0,0,-1)
-scene:add_child(ice)
+ric = gr.algebraic('infinite cylinder', 'x^2 + z^2 - 1', 2)
+ric:set_material(red)
+rbp = gr.algebraic('biplane', 'y^2 - 1', 2)
+rbp:set_material(red)
+rcyl = gr.csg('cylinder', ric, '*', rbp, 1.42)
+
+gic = gr.algebraic('infinite cylinder', 'x^2 + z^2 - 1', 2)
+gic:set_material(green)
+gbp = gr.algebraic('biplane', 'y^2 - 1', 2)
+gbp:set_material(green)
+gcyl = gr.csg('cylinder', gic, '*', gbp, 1.42)
+
+bic = gr.algebraic('infinite cylinder', 'x^2 + z^2 - 1', 2)
+bic:set_material(blue)
+bbp = gr.algebraic('biplane', 'y^2 - 1', 2)
+bbp:set_material(blue)
+bcyl = gr.csg('cylinder', bic, '*', bbp, 1.42)
+
+yic = gr.algebraic('infinite cylinder', 'x^2 + z^2 - 1', 2)
+yic:set_material(yellow)
+ybp = gr.algebraic('biplane', 'y^2 - 1', 2)
+ybp:set_material(yellow)
+ycyl = gr.csg('cylinder', yic, '*', ybp, 1.42)
+
+-------------------
+-- Helix
+
+helix = gr.node('helix')
+
+dir  = 0
+ddir = 20
+h    = 15
+dh   = 1.8
+sep  = 3
+w    = 0.15
+
+for i = -h,h,dh do
+  line = gr.node('line')
+
+  b1 = gr.sphere('b1')
+  b1:set_material(db)
+  b1:translate(sep,0,0)
+  line:add_child(b1)
+
+  b2 = gr.sphere('b2')
+  b2:set_material(lb)
+  b2:translate(-sep,0,0)
+  line:add_child(b2)
+
+  col = math.random(4)
+  q1 = gr.node('q1')
+  q2 = gr.node('q2')
+  if col == 1 then 
+    q1:add_child(gcyl)
+    q2:add_child(rcyl)
+  end
+  if col == 2 then 
+    q1:add_child(rcyl)
+    q2:add_child(gcyl)
+  end
+  if col == 3 then
+    q1:add_child(ycyl)
+    q2:add_child(bcyl)
+  end
+  if col == 4 then
+    q1:add_child(bcyl)
+    q2:add_child(ycyl)
+  end
+
+  q1:translate(0,1,0)
+  q1:scale(w,sep/2,w)
+  q1:rotate('Z', 90)
+  line:add_child(q1)
+
+  q2:translate(0,1,0)
+  q2:scale(w,sep/2,w)
+  q2:rotate('Z',-90)
+  line:add_child(q2)
+
+  line:translate(0,i,0)
+  line:rotate('Y', dir)
+  dir = dir + ddir
+
+  helix:add_child(line)
+end
 
 -------------------
 -- Render
 
-can1 = gr.node('can1')
-can1:add_child(can)
-can1:translate(-2,0,0)
-scene:add_child(can1)
+helix:rotate('Z',-90)
+scene:add_child(helix)
+scene:translate(0,0,-30)
 
-can2 = gr.node('can2')
-can2:add_child(can)
-can2:rotate('Y',20)
-can2:rotate('X',-90)
-can2:rotate('Y',60)
-can2:translate(2.5,1,1)
-scene:add_child(can2)
+white_light = gr.light({2,2,1}, {1.0, 1.0, 1.0}, {1, 0, 0})
+gr.render(scene, 'dna.png', 512, 192,
+	  {0,0,0}, {0, 0, -1}, {0, 1, 0}, 50,
+	  {0.3, 0.3, 0.3}, {white_light})
 
-scene:rotate('X',30)
-scene:translate(0,-1.5,-8)
-
-gr.render(scene,
-	  'coke.png', 1024, 576,
-	  {0, 0, 0}, {0, 0, -1}, {0, 1, 0}, 50,
-	  {0.4, 0.4, 0.4}, {gr.light({200, 202, 430}, {0.8, 0.8, 0.8}, {1, 0, 0})})
